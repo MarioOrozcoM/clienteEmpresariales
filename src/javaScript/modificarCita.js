a5182e22875c8b86c19f413bc92b48019bbe3c3c
C.M Bernal Cuellar <2420201003@estudiantesunibague.edu.co>
2024-04-19 17:32:09 -0500

    fixed

--- src/javaScript/modificarCita.js
+++ src/javaScript/modificarCita.js
@@ -1,33 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Coloca aquí tu código JavaScript
-    document.getElementById('buscarCitaForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        var identificacion = document.getElementById('identificacion').value;
-        //Necesito lo del back
-        fetch(`http://localhost:8080/citas/${identificacion}`)
-        .then(response => response.json())
-        .then(data => {
-            // Maneja los datos recibidos del backend aquí
-            console.log('Datos recibidos del backend:', data);
-            mostrarResultado(data);
-        })
-        .catch(error => {
-            console.error('Error al buscar cita:', error);
-        });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buscarCitaForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var identificacion = document.getElementById('identificacion').value;
+
+    fetch(`http://localhost:8080/citas/${identificacion}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Datos recibidos del backend:', data);
+        mostrarResultado(data);
+      })
+      .catch(error => {
+        console.error('Error al buscar cita:', error);
+      });
+  });
 });
 
 function mostrarResultado(data) {
-    if (data) {
-        console.log('Cita encontrada:', data);
-        // Redirige al usuario al formulario de modificación de cita con los detalles de la cita cargados en él
-        window.location.href = '/src/modificarCitaForm.html?id=' + data.numeroIdentificacion;
-
-    } else {
-        console.log('La cita no fue encontrada');
-    }
+  if (data) {
+    console.log('Cita encontrada:', data);
+    window.location.href = '/src/modificarCitaForm.html?id=' + data.numeroIdentificacion;
+  } else {
+    console.log('La cita no fue encontrada');
+  }
 }
-
-
-
