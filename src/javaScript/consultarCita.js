c57b7349b259eea1ea114dc05cd3b000a28fd03b
C.M Bernal Cuellar <2420201003@estudiantesunibague.edu.co>
2024-04-24 08:31:54 -0500

    consultar cita y consultorio

---
+++ src/javaScript/consultarCita.js
@@ -0,0 +1,27 @@
+function mostrarListaCita() {
+    fetch('http://localhost:8080/citas/todas-las-citas')
+        .then(response => response.json())
+        .then(data => {
+            const tablaBody = document.getElementById('tablaPacientesBody');
+            tablaBody.innerHTML = ''; // Limpiar la tabla antes de llenarla con los datos
+            data.forEach(cita => {
+                const fila = document.createElement('tr');
+                fila.innerHTML = `
+                    <td class="border px-4 py-2">${cita.numeroIdentificacion}</td>
+                    <td class="border px-4 py-2">${cita.nombrePaciente}</td>
+                    <td class="border px-4 py-2">${cita.fecha}</td>
+                    <td class="border px-4 py-2">${cita.tipoCita}</td>
+                    <td class="border px-4 py-2">${cita.costo}</td>
+                    <td class="border px-4 py-2">${cita.idConsultorio}</td>
+  
+                `;
+                tablaBody.appendChild(fila);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener datos de citas:', error);
+        });
+  }
+  
+  // Llamada a la función para mostrar la lista de citas y sus respectivos consultorios
+  mostrarListaCita();
--- src/javaScript/consultarConsultorio.js
+++ src/javaScript/consultarConsultorio.js
@@ -1,41 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btnAceptar = document.getElementById('btnAceptar');
-
-    btnAceptar.addEventListener('click', function() {
-        const filtroID = document.getElementById('filtroID').value;
-
-        // Realizar la solicitud al backend para obtener el consultorio por ID
-        fetch(`http://localhost:8080/consultorios/${filtroID}`)
-            .then(response => response.json())
-            .then(data => {
-                // Limpiar la tabla de consultorios
-                const tablaConsultoriosBody = document.getElementById('tablaConsultoriosBody');
-                tablaConsultoriosBody.innerHTML = '';
-
-                // Verificar si se encontró el consultorio
-                if (data) {
-                    // Crear una fila para mostrar el consultorio encontrado
-                    const newRow = document.createElement('tr');
-                    newRow.innerHTML = `
-                        <td class="px-4 py-2 border">${data.id}</td>
-                        <td class="px-4 py-2 border">${data.nombre}</td>
-                    `;
-
-                    // Agregar la fila a la tabla
-                    tablaConsultoriosBody.appendChild(newRow);
-                } else {
-                    // Mostrar un mensaje indicando que no se encontró el consultorio
-                    const newRow = document.createElement('tr');
-                    newRow.innerHTML = `
-                        <td class="px-4 py-2 border" colspan="2">No se encontró ningún consultorio con ese ID</td>
-                    `;
-
-                    // Agregar la fila a la tabla
-                    tablaConsultoriosBody.appendChild(newRow);
-                }
-            })
-            .catch(error => {
-                console.error('Error al obtener el consultorio:', error);
+function mostrarListaConsultorios() {
+    fetch('http://localhost:8080/consultorios')
+        .then(response => response.json())
+        .then(data => {
+            const tablaBody = document.getElementById('tablaConsultoriosBody');
+            tablaBody.innerHTML = ''; // Limpiar la tabla antes de llenarla con los datos
+            data.forEach(consultorio => {
+                const fila = document.createElement('tr');
+                fila.innerHTML = `
+                    <td class="border px-4 py-2">${consultorio.id}</td>
+                    <td class="border px-4 py-2">${consultorio.nombre}</td>
+                `;
+                tablaBody.appendChild(fila);
             });
-    });
-});
+        })
+        .catch(error => {
+            console.error('Error al obtener datos de citas:', error);
+        });
+  }
+  
+  // Llamada a la función para mostrar la lista de citas y sus respectivos consultorios
+  mostrarListaConsultorios();
